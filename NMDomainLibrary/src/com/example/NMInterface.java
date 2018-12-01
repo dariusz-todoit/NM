e34fe8d13e754d57d4d39fb395f14004cf0a199f
dariusz-todoit <tarakena@gmail.com>
2018-12-01 11:04:51 +0100

    first commit of NMInterface.java

---
+++ NMDomainLibrary/src/com/example/NMInterface.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import java.util.List;
+
+public interface NMInterface {
+  void deleteNote (int id);
+  void updateNote (int id, String text);
+  int newNote (String text, int projectId);
+  List<Object[]> getNotes ();  
+}
